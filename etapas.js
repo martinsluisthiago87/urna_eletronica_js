ced017f3424728e2761e449a2c7c39bd482c98f1
Thiago Martins <martins.luisthiago@gmail.com>
2020-09-26 22:01:57 -0300

    Primeiro commit do projeto urna eletronica

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numero: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulano de tal',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'boy.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Maria da silva',
+                partido: 'DEFG',
+                fotos: [
+                    { url: 'boy.png', legenda: 'Vereador'}
+                ] 
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Jose Alves',
+                partido: 'ABC',
+                vice: 'Leo',
+                fotos:[
+                    { url: 'boy.png', legenda: 'Prefeito'},
+                    { url: 'boy.png', legenda: 'Vice-Prefeito', small: true},
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Claudio Novo',
+                partido: 'DFG',
+                vice: 'Cacau',
+                fotos: [
+                    { url: 'boy.png', legenda: 'Prefeito' },
+                    { url: 'boy.png', legenda: 'Vice-Prefeito', small: true },
+                ]
+            },
+        ]
+    }
+]
